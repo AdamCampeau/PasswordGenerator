80716224bf97338fe148060964dc16d543597d95
AdamCampeau <adamcampeau3@gmail.com>
2021-12-15 12:02:43 -0500

    ta help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,58 @@
 // Assignment code here
-var passwordText='';
+function generatePassword() {
+  var password='';
+  var characters = '';
+  
+  const passwordCharSet = {
+    uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+    lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    symbols: ["!","@","#","$","%","^","&","*","(",")","_","+","<",">","/","?"],
+    numbers: ["1","2","3","4","5","6","7","8","9","0"],
+  };
+  
+  var lengthPrompt= prompt ('Please select a valid password length, must be between 8 - 128 characters')
+  while (lengthPrompt < 8 || lengthPrompt > 128) {
+    alert('Your password must be a number between 8 and 128 characters')
+    lengthPrompt= prompt ('Please select a valid password length, must be between 8 - 128 characters')
+  }
 
-const passwordCharSet = {
-  uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-  lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-  symbols: ["!","@","#","$","%","^","&","*","(",")","_","+","<",">","/","?"],
-  numbers: ["1","2","3","4","5","6","7","8","9","0"],
-};
+  var uppercasePrompt=confirm ('Would you like to user upper case characters?');
+  var lowercasePrompt=confirm ('Would you like to use lower case characters?');
+  var symbolsPrompt=confirm ('Would you like to include symbols?');
+  var numbersPrompt=confirm ('Would you like to include numbers?');
 
-var uppercasePrompt=confirm ('Would you like to user upper case characters?');
-var lowercasePrompt=confirm ('Would you like to use lower case characters?');
-var symbolsPrompt=confirm ('Would you like to include symbols?');
-var numbersPrompt=confirm ('Would you like to include numbers?');
-var lengthPrompt= prompt ('Please select a valid password length, must be between 8 - 128 characters')
+  while(uppercasePrompt === false && lowercasePrompt === false && symbolsPrompt === false && numbersPrompt === false) {
+    alert("You must choose at least one character type")
+    uppercasePrompt=confirm ('Would you like to user upper case characters?');
+    lowercasePrompt=confirm ('Would you like to use lower case characters?');
+    symbolsPrompt=confirm ('Would you like to include symbols?');
+    numbersPrompt=confirm ('Would you like to include numbers?');
+  }
+  
+  //if prompt is true,
+  if (uppercasePrompt === true) {
+    characters += passwordCharSet.uppercase.join('')
+  }
+  
+  if (lowercasePrompt === true) {
+    characters += passwordCharSet.lowercase.join('')
+  }
+  
+  if (symbolsPrompt === true) {
+    characters += passwordCharSet.symbols.join('')
+  }
+  
+  if (numbersPrompt === true) {
+    characters += passwordCharSet.numbers.join('')
+  }
 
-//if prompt is true,
-if (uppercasePrompt === true) {
-  passwordCharSet.uppercase + passwordText
+  for (var i = 0; i < lengthPrompt; i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+
+ return password
+  
 }
-
-if (lowercasePrompt === true) {
-  passwordCharSet.lowercase + passwordText
-}
-
-if (symbolsPrompt === true) {
-  passwordCharSet.symbols + passwordText
-}
-
-if (numbersPrompt === true) {
-  passwordCharSet.numbers + passwordText
-}
-
-// input from prompt sets length of password
-let lengthPrompt=passwordText.length;
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
